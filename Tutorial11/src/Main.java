e2afa70fb9d9d854a6235a84bf328f4dea9cd658
oldbbie <tjd0725@naver.com>
2021-09-20 00:22:08 +0900

    update Tutorial11 for ver2

--- Tutorial11/src/Main.java
+++ Tutorial11/src/Main.java
@@ -1,19 +1,33 @@
 
+//public class Main {
+//
+//	public static int factorial(int number) {
+//		int result = 1;
+//		for(int i = 2; i<=number; i++)
+//		{
+//			result *= i;
+//		}
+//		return result;
+//	}
+//	
+//	public static void main(String[] args) {
+//
+//		System.out.println("10의 팩토리얼은 " + factorial(10));
+//
+//	}
+//
+//}
+
 public class Main {
 
 	public static int factorial(int number) {
-		int result = 1;
-		for(int i = 2; i<=number; i++)
-		{
-			result *= i;
-		}
-		return result;
+		if(number == 1)
+			return 1;
+		else
+			return number * factorial(number-1);
 	}
 	
 	public static void main(String[] args) {
-
-		System.out.println("10의 팩토리얼은 " + factorial(10));
-
+		System.out.println("10 팩토리얼은 " + factorial(10));
 	}
-
 }
