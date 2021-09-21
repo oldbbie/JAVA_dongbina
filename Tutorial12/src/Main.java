59195729313648228dd0a7247a1da6c2adbbdba3
oldbbie <tjd0725@naver.com>
2021-09-22 01:43:06 +0900

    End Tutorial12

--- Tutorial12/src/Main.java
+++ Tutorial12/src/Main.java
@@ -1,20 +1,35 @@
 
-public class Main {
+//public class Main {
+//
+//	public static int fibonacci(int number) {
+//		int one = 1;
+//		int two = 1;
+//		int result = -1;
+//		if(number == 1) return one;
+//		else if(number == 2) return two;
+//		else {
+//			for(int i = 2; i < number; i++) {
+//				result = one + two;
+//				one = two;
+//				two = result;
+//			}
+//			return result;
+//		}
+//	}
+//	
+//	public static void main(String[] args) {
+//		
+//		System.out.println("피보나치 수열의 10번째 원소는" + fibonacci(10) +"입니다.");
+//	}
+//
+//}
 
+public class Main {
+// 이런 재귀함수는 오래걸린다. 그래서 동적프로그래밍을 이용해 이러한 문제를 해결할 수 있다.
 	public static int fibonacci(int number) {
-		int one = 1;
-		int two = 1;
-		int result = -1;
-		if(number == 1) return one;
-		else if(number == 2) return two;
-		else {
-			for(int i = 2; i < number; i++) {
-				result = one + two;
-				one = two;
-				two = result;
-			}
-			return result;
-		}
+		if(number == 1) return 1;
+		else if(number == 2) return 1;
+		else return fibonacci(number-1) + fibonacci(number-2);
 	}
 	
 	public static void main(String[] args) {
