d9923ec68899137a850539cc4052b5a5be8a51c1
oldbbie <tjd0725@naver.com>
2021-10-01 01:42:27 +0900

    Start Tutorial16

---
+++ Tutorial16/src/Main.java
@@ -0,0 +1,13 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+	
+		Student student1 = new Student("홍길동",20, 175, 70,"20170101",1,4.5);
+		Student student2 = new Student("이순신",30, 170, 70,"20090505",1,3.5);
+
+		student1.show();
+		student2.show();
+	}
+
+}
